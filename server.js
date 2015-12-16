82d3b806a1d1e0ba41aa450ef35181c7c556bf83
David Burgstahler <davidburgstahler@MacBook-Pro-2.local>
2015-12-16 14:04:04 -0500

    server routes

--- server.js
+++ server.js
@@ -15,6 +15,87 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.get('/users',function(req,res) {
+
+});
+
+app.post('/register',function(req,res) {
+
+});
+
+app.post('/login',function(req,res) {
+
+});
+
+app.get('/users/:id',function(req,res) {
+
+});
+
+app.put('/users/:id',function(req,res) {
+
+});
+
+app.delete('/users/:id',function(req,res) {
+
+});
+
+app.get('/clubs',function(req,res) {
+
+});
+
+app.post('/clubs',function(req,res) {
+
+});
+
+app.get('/clubs/:id',function(req,res) {
+
+});
+
+app.put('/clubs/:id',function(req,res) {
+
+});
+
+app.delete('/clubs/:id',function(req,res) {
+
+});
+
+app.get('/clubs/events',function(req,res) {
+
+});
+
+
+app.get('/clubs/:id/events',function(req,res) {
+
+});
+
+app.put('/clubs/:id/events',function(req,res) {
+
+});
+
+app.post('/clubs/:id/events',function(req,res) {
+
+});
+
+app.get('/clubs/announcements',function(req,res) {
+
+});
+
+app.get('/clubs/:id/announcements',function(req,res) {
+
+});
+
+app.put('/clubs/:id/announcements',function(req,res) {
+
+});
+
+app.post('/clubs/:id/announcements',function(req,res) {
+
+});
+
+app.put('/clubs/:id/request',function(req,res) {
+
+});
+
 var server = app.listen(process.env.PORT || 4000, function() {
   var host = server.address().address;
   var port = server.address().port;
